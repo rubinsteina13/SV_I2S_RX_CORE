61cccc8bd8425c7011b9fbc7057da258d7ed3fcd
Serhii Yatsenko <royalroad1995@gmail.com>
2020-06-03 13:03:34 +0300

    Update decoder_module.sv

--- decoder_module.sv
+++ decoder_module.sv
@@ -1,21 +1,43 @@
-//////////////////////////////////////////////////////////////////
-// Name File	: decoder_module.sv								//
-// Autor		: Serhii Yatsenko								//
-// Mail			: royalroad1995@gmail.com						//
-// Description	: SystemVerilog module of the					//
-// 				  Binary to Decimal Decoder						//
-//				  (Synthesizable Digital Core)					//
-// Date			: 2020											//
-// License		: MIT											//
-//////////////////////////////////////////////////////////////////
-
+/**
+  ***********************************************************************************
+  * @file    decoder_module.sv
+  * @author  Serhii Yatsenko [royalroad1995@gmail.com]
+  * @version V1.0
+  * @date    May-2020
+  * @brief   Synthesizable Digital Core of the Binary to Decimal Decoder
+  ***********************************************************************************
+  * @license
+  *
+  * MIT License
+  *
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to deal
+  * in the Software without restriction, including without limitation the rights
+  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in all
+  * copies or substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  * SOFTWARE.
+  *
+  ***********************************************************************************
+  */
 module decoder_module #(
-	parameter	INPUT_WIDTH		= 5,
-					OUTPUT_WIDTH	= 32
+	parameter	INPUT_WIDTH	= 5,
+			OUTPUT_WIDTH	= 32
 )(	
-	input		[INPUT_WIDTH-1:0]		d_i,
+	input	[INPUT_WIDTH-1:0]	d_i,
 	output	[OUTPUT_WIDTH-1:0]	d_o
 );
+	
 	assign d_o = 1'b1 << d_i;
 	
 endmodule: decoder_module
