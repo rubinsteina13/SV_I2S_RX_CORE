c2b3142e7c1e391d99be800794b1c376d1c8a377
Serhii Yatsenko <royalroad1995@gmail.com>
2020-06-03 13:09:18 +0300

    Update full_sync_dff_module.sv

--- full_sync_dff_module.sv
+++ full_sync_dff_module.sv
@@ -1,22 +1,44 @@
-//////////////////////////////////////////////////////////////////
-// Name File	: full_sync_dff_module.sv						//
-// Autor		: Serhii Yatsenko								//
-// Mail			: royalroad1995@gmail.com						//
-// Description	: SystemVerilog module of the					//
-// 				  Synchronous D-trigger							//
-//				  (Synthesizable Digital Core).					//
-//				  Synchronous D-trigger use synchronous reset	// 
-//				  and enable inputs, where reset overrules the	// 
-//				  enable input.									//
-// Date			: 2020											//
-// License		: MIT											//
-//////////////////////////////////////////////////////////////////
+/**
+  ***********************************************************************************
+  * @file    full_sync_dff_module.sv
+  * @author  Serhii Yatsenko [royalroad1995@gmail.com]
+  * @version V1.0
+  * @date    May-2020
+  * @brief   Synthesizable Digital Core of the Synchronous D-trigger.
+  *	     Synchronous D-trigger use synchronous reset and enable inputs, where
+  *	     reset overrules the enable input.
+  ***********************************************************************************
+  * @license
+  *
+  * MIT License
+  *
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to deal
+  * in the Software without restriction, including without limitation the rights
+  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in all
+  * copies or substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  * SOFTWARE.
+  *
+  ***********************************************************************************
+  */
 
 module full_sync_dff_module
 (
-	input		logic d_i, rst_i, en_i, clk_i,
+	input	logic d_i, rst_i, en_i, clk_i,
 	output	logic q_o		
 );
+	
 	always_ff @(posedge clk_i) begin
 		if(rst_i)
 			q_o <= 0;
